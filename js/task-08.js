23e7b05c353b441c1d7d266aa75e688b196f18b8
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-15 19:27:15 +0300

    task8+

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+// отримуємо доступ до форми
+const formEl = document.querySelector('.login-form');
 
+// додаємо івент-лісенер
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+    // забороняємо дефолтну поведінку
+    event.preventDefault();
+
+    // змінні для доступу до введених даних
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    // якщо полк пусте - виводимо алерт
+    if (formElements.email.value === '' || formElements.password.value === '') {
+        alert('Будь ласка, заповніть всі поля');
+        return
+    }
+    // передаємо дані у об'єкт
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData)
+    // перезавантажуємо інпут
+    event.currentTarget.reset();
+};
