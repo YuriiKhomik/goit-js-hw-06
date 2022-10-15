637f7c0dfddec0e5d21391ea58e3fc47bc3d2c74
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-15 20:25:47 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// отримуємо доступ до елементів HTML
+const changeColorButton = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const textEl = document.querySelector('.color');
+
+
+// додаємо івент-лісенер
+changeColorButton.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  const randomColor = function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+  // додаємо згенероавний рандомний колір в інлайн-стиль елемента body 
+  bodyEl.style.backgroundColor = randomColor();
+  // додаємо значення кольору в span
+  textEl.textContent = bodyEl.style.backgroundColor;
+};
+
+
+
+
