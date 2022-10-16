16090ec317fe02554e15b1ea05b1f6c5f4928e39
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-17 00:02:08 +0300

    task 5 edited

--- js/task-05.js
+++ js/task-05.js
@@ -1,22 +1,20 @@
 // отримуємо доступ до елементів
 
 const inputEl = document.querySelector('#name-input');
-const titleEl = document.querySelector("#name-output");
+const titleEl = document.querySelector('#name-output');
 
 // додаємо івент лістенер на інпут і колбек для зміни аутпута
 inputEl.addEventListener('input', onInputChange);
 
-function onInputChange(event) {
-    titleEl.textContent = event.currentTarget.value;
-};
-
-
-
-
 // function onInputChange(event) {
-//     if (event.currentTarget.value !== '') {
-//         titleEl.textContent = event.currentTarget.value;
-//     } else {
-//         titleEl.textContent = 'Anonymous';
-//     };
+//     titleEl.textContent = event.currentTarget.value;
 // };
+
+
+function onInputChange(event) {
+    if (event.currentTarget.value !== '') {
+        titleEl.textContent = event.currentTarget.value;
+    } else {
+        titleEl.textContent = 'Anonymous';
+    };
+};
